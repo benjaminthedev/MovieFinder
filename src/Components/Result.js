d94aba13d1fbd7903c02738c04c2dd3c56ca8bd3
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-01-13 06:56:29 +0000

    updated project

---
+++ src/Components/Result.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Result from './Results'
+
+function Result({result}) {
+    return (
+        <div className="result">
+            <img src={result.Poster} />
+            <h3>{result.Title}</h3>
+        </div>
+    )
+}
+
+export className="result" default Result
+{result}
